c1925868123e4fc75577b24914a358d67869e6c1
Manish Dixit <girijason08@gmail.com>
2025-09-13 13:55:40 -0500

    dynamically increments request number

--- DependencyInjectionSample/Controllers/OperationsController.cs
+++ DependencyInjectionSample/Controllers/OperationsController.cs
@@ -6,35 +6,22 @@ namespace DependencyInjectionSample.Controllers;
 
 public class OperationsController : Controller
 {
-	private readonly OperationService _operationService;
-	private readonly IOperationTransient _transientOperation;
-	private readonly IOperationScoped _scopedOperation;
-	private readonly IOperationSingleton _singletonOperation;
-	private readonly IOperationSingletonInstance _singletonInstanceOperation;
+    private static int _requestNumber = 0;
 
-	public OperationsController(OperationService operationService,
-		IOperationTransient transientOperation,
-		IOperationScoped scopedOperation,
-		IOperationSingleton singletonOperation,
-		IOperationSingletonInstance singletonInstanceOperation)
-	{
-		_operationService = operationService;
-		_transientOperation = transientOperation;
-		_scopedOperation = scopedOperation;
-		_singletonOperation = singletonOperation;
-		_singletonInstanceOperation = singletonInstanceOperation;
-	}
-
-	public IActionResult Index()
-	{
-		// ViewBag contains controller-requested services
-		ViewBag.Transient = _transientOperation;
-		ViewBag.Scoped = _scopedOperation;
-		ViewBag.Singleton = _singletonOperation;
-		ViewBag.SingletonInstance = _singletonInstanceOperation;
-
-		// Operation service has its own requested services
-		ViewBag.Service = _operationService;
-		return View();
-	}
+    public IActionResult Index(
+        [FromServices] OperationService service,
+        [FromServices] IOperationTransient transient,
+        [FromServices] IOperationScoped scoped,
+        [FromServices] IOperationSingleton singleton,
+        [FromServices] IOperationSingletonInstance instance)
+    {
+        _requestNumber++;
+        ViewBag.RequestNumber = _requestNumber;
+        ViewBag.Service = service;
+        ViewBag.Transient = transient;
+        ViewBag.Scoped = scoped;
+        ViewBag.Singleton = singleton;
+        ViewBag.SingletonInstance = instance;
+        return View();
+    }
 }
